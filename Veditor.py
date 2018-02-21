1c86180c19462ba4dd967875be1214b023e80b59
Joseph Vargas <vargman54@gmail.com>
2018-02-21 12:07:00 -0600

    Encapsulated application into class for spawning new windows

--- Veditor.py
+++ Veditor.py
@@ -2,61 +2,61 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 
-def save_file(textbox):
-    filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
+class Veditor(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.master.title("Veditor")
+        #text area
+        self.text = tk.Text(master, bg="darkgrey", fg="white")
+        self.text.grid(sticky="nsew")
+        #Menu Code
+        self.menubar = tk.Menu(master)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="New", command= lambda: spawn_new(master))
+        self.filemenu.add_command(label="Open", command= lambda: open_file(self.text))
+        self.filemenu.add_command(label="Save", command= lambda: save_file(self.text))
+
+        self.filemenu.add_separator()
+
+        self.filemenu.add_command(label="Delete System32", command=master.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+
+        master.columnconfigure(0, weight=1)
+        master.rowconfigure(0, weight=1)
+        master.config(menu=self.menubar)
+
+        def save_file(textbox):
+            filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
                                                  filetypes=(("text files", "*.txt"), ("all files", "*.*")))
-    if filename != "":
-        write_out(get_input(textbox), filename)
+            if filename != "":
+                write_out(get_input(textbox), filename)
 
-def open_file(textbox):
-    filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+        def open_file(textbox):
+            filename = filedialog.askopenfilename(initialdir="/", title="Select file",
                                                  filetypes=(("text files", "*.txt"), ("all files", "*.*")))
-    write_in(filename, textbox)
+            write_in(filename, textbox)
 
-def write_in(filename, textbox):
-    f = open(filename, 'r')
-    contents = f.read()
-    textbox.insert(INSERT, contents)
+        def write_in(filename, textbox):
+            f = open(filename, 'r')
+            contents = f.read()
+            textbox.insert(INSERT, contents)
 
-def write_out(contents, fileName):
-    try:
-        f = open(fileName, 'w')
-        f.write(contents)
-    except FileNotFoundError:
-        return
-
-def get_input(textbox):
-    input = textbox.get("1.0",'end-1c')
-    return input
-
-def donothing():
-   
-   filewin = tk.Toplevel(root)
-   button = tk.Button(filewin, text="Do nothing button")
-   button.pack()
-
-root=tk.Tk("text editor")
-root.title("Veditor")
-
-#stuff goes here
-text = tk.Text(root, bg="darkgrey", fg="white")
-text.grid(sticky="nsew")
+        def write_out(contents, fileName):
+            try:
+                f = open(fileName, 'w')
+                f.write(contents)
+            except FileNotFoundError:
+                return
+        def get_input(textbox):
+            input = textbox.get("1.0",'end-1c')
+            return input
+            
+        def spawn_new(master):
+            root = tk.Tk()
+            new = Veditor(root)
 
 
-#Menu code
-menubar = tk.Menu(root)
-filemenu = tk.Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=donothing)
-filemenu.add_command(label="Open", command= lambda: open_file(text))
-filemenu.add_command(label="Save", command= lambda: save_file(text))
-
-filemenu.add_separator()
-
-filemenu.add_command(label="Delete System32", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-root.config(menu=menubar)
+root = tk.Tk()
+app = Veditor(root)
 root.mainloop()
